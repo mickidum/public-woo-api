9a81f19e4a95c4313422204f2b910a09ce6f83ff
Michael <michael@zur.local>
2019-05-23 16:58:11 +0300

    add endpoints

--- includes/class-public-woo-api-endpoints.php
+++ includes/class-public-woo-api-endpoints.php
@@ -1,7 +1,8 @@
 <?php
 
 /**
- * mywoo class
+ * Public_Woo_Api_Endpoints class
+ * Here is all enpoints with their callback functions
  */
 
 use Automattic\WooCommerce\Client;
@@ -39,9 +40,49 @@ class Public_Woo_Api_Endpoints
         'callback' => array($this, 'view_products')
     ));
 
-    register_rest_route( $this->endpoint, '/products'. '/(?P<id>[\d]+)', array(
+    register_rest_route( $this->endpoint, '/products' . '/(?P<id>[\d]+)', array(
         'methods' => 'GET',
         'callback' => array($this, 'view_product')
+    ));
+
+    register_rest_route( $this->endpoint, '/products' . '/(?P<id>[\d]+)' . '/variations', array(
+        'methods' => 'GET',
+        'callback' => array($this, 'view_product_variations')
+    ));
+
+    register_rest_route( $this->endpoint, '/products' . '/(?P<id>[\d]+)' . '/variations' . '/(?P<v_id>[\d]+)', array(
+        'methods' => 'GET',
+        'callback' => array($this, 'view_product_variation')
+    ));
+
+    register_rest_route( $this->endpoint, '/products/categories', array(
+        'methods' => 'GET',
+        'callback' => array($this, 'view_products_categories')
+    ));
+
+    register_rest_route( $this->endpoint, '/products/categories' . '/(?P<id>[\d]+)', array(
+        'methods' => 'GET',
+        'callback' => array($this, 'view_products_category')
+    ));
+
+    register_rest_route( $this->endpoint, '/products/tags', array(
+        'methods' => 'GET',
+        'callback' => array($this, 'view_products_tags')
+    ));
+
+    register_rest_route( $this->endpoint, '/products/tags' . '/(?P<id>[\d]+)', array(
+        'methods' => 'GET',
+        'callback' => array($this, 'view_products_tag')
+    ));
+
+    register_rest_route( $this->endpoint, '/products/reviews', array(
+        'methods' => 'GET',
+        'callback' => array($this, 'view_products_reviews')
+    ));
+
+    register_rest_route( $this->endpoint, '/products/reviews' . '/(?P<id>[\d]+)', array(
+        'methods' => 'GET',
+        'callback' => array($this, 'view_products_review')
     ));
 	}
 
@@ -63,9 +104,144 @@ class Public_Woo_Api_Endpoints
 
 	public function view_product( $request ) {
 		$id = $request->get_param('id');
+
+		if ( isset( $request['_embed'] ) )  {
+			return new WP_Error(500, 'there are some bugs this _embed parameter, don\'t use it');
+		}
+
 		try {
 			$product = $this->woocommerce->get( 'products/' . $id);
 			return $product;
+		} catch (HttpClientException $e) {
+			return new WP_Error($e->getCode(), $e->getMessage());
+		}
+	}
+
+	public function view_product_variations( $request ) {
+
+		$id = $request->get_param('id');
+		$filters = $request->get_query_params();
+
+		if ( isset( $request['_embed'] ) )  {
+			return new WP_Error(500, 'there are some bugs this _embed parameter, don\'t use it');
+		}
+		
+		try {
+			$variations = $this->woocommerce->get( 'products/' . $id . '/variations', $filters);
+			return $variations;
+		} catch (HttpClientException $e) {
+			return new WP_Error($e->getCode(), $e->getMessage());
+		}
+	}
+
+	public function view_product_variation( $request ) {
+
+		$id = $request->get_param('id');
+		$v_id = $request->get_param('v_id');
+
+		if ( isset( $request['_embed'] ) )  {
+			return new WP_Error(500, 'there are some bugs this _embed parameter, don\'t use it');
+		}
+
+		try {
+			$variation = $this->woocommerce->get( 'products/' . $id . '/variations/' . $v_id);
+			return $variation;
+		} catch (HttpClientException $e) {
+			return new WP_Error($e->getCode(), $e->getMessage());
+		}
+	}
+
+	public function view_products_categories($request) {
+		
+		$filters = $request->get_query_params();
+		
+		if ( isset( $request['_embed'] ) )  {
+			return new WP_Error(500, 'there are some bugs this _embed parameter, don\'t use it');
+		}
+
+		try {
+			$categories = $this->woocommerce->get( 'products/categories', $filters );
+			return $categories;
+		} catch (HttpClientException $e) {
+			return new WP_Error($e->getCode(), $e->getMessage());
+		}
+	}
+
+	public function view_products_category($request) {
+
+		$id = $request->get_param('id');
+		
+		if ( isset( $request['_embed'] ) )  {
+			return new WP_Error(500, 'there are some bugs this _embed parameter, don\'t use it');
+		}
+
+		try {
+			$category = $this->woocommerce->get( 'products/categories/' . $id );
+			return $category;
+		} catch (HttpClientException $e) {
+			return new WP_Error($e->getCode(), $e->getMessage());
+		}
+	}
+
+	public function view_products_tags($request) {
+		
+		$filters = $request->get_query_params();
+		
+		if ( isset( $request['_embed'] ) )  {
+			return new WP_Error(500, 'there are some bugs this _embed parameter, don\'t use it');
+		}
+
+		try {
+			$tags = $this->woocommerce->get( 'products/tags', $filters );
+			return $tags;
+		} catch (HttpClientException $e) {
+			return new WP_Error($e->getCode(), $e->getMessage());
+		}
+	}
+
+	public function view_products_tag($request) {
+
+		$id = $request->get_param('id');
+		
+		if ( isset( $request['_embed'] ) )  {
+			return new WP_Error(500, 'there are some bugs this _embed parameter, don\'t use it');
+		}
+
+		try {
+			$tag = $this->woocommerce->get( 'products/tags/' . $id );
+			return $tag;
+		} catch (HttpClientException $e) {
+			return new WP_Error($e->getCode(), $e->getMessage());
+		}
+	}
+
+	public function view_products_reviews($request) {
+		
+		$filters = $request->get_query_params();
+		
+		if ( isset( $request['_embed'] ) )  {
+			return new WP_Error(500, 'there are some bugs this _embed parameter, don\'t use it');
+		}
+
+		try {
+			$reviews = $this->woocommerce->get( 'products/reviews', $filters );
+			return $reviews;
+		} catch (HttpClientException $e) {
+			return new WP_Error($e->getCode(), $e->getMessage());
+		}
+	}
+
+	public function view_products_review($request) {
+
+		$id = $request->get_param('id');
+		
+		if ( isset( $request['_embed'] ) )  {
+			return new WP_Error(500, 'there are some bugs this _embed parameter, don\'t use it');
+		}
+
+		try {
+			$review = $this->woocommerce->get( 'products/reviews/' . $id );
+			return $review;
 		} catch (HttpClientException $e) {
 			return new WP_Error($e->getCode(), $e->getMessage());
 		}
